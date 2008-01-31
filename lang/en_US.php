66ba80518bfed10fc6f417c4812adf81bf08f5e7
chillu <ingo@silverstripe.com>
2008-01-31 02:41:05 +0000

    added lang

---
+++ lang/en_US.php
@@ -0,0 +1,18 @@
+<?php
+
+global $lang;
+
+$lang['en_US']['RecaptchaField']['EMPTY'] = array(
+	'Please answer the captcha question',
+	
+						PR_MEDIUM,
+	'Recaptcha (http://recaptcha.net) provides two words in an image, and expects a user to type them in a textfield'
+);
+$lang['en_US']['RecaptchaField']['VALIDSOLUTION'] = array(
+	'Your answer didn\'t match the question, please try again',
+	
+						PR_MEDIUM,
+	'Recaptcha (http://recaptcha.net) provides two words in an image, and expects a user to type them in a textfield'
+);
+
+?>
