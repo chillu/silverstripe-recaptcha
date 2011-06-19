a241d60bbf2dc40b60252383a264b9c702d732b2
Philipp Krenn <pk@xeraa.net>
2011-06-19 14:35:01 +0200

    German translation added

---
+++ lang/de_DE.php
@@ -0,0 +1,16 @@
+<?php
+
+global $lang;
+
+$lang['de_DE']['RecaptchaField']['EMPTY'] = array(
+	'Bitte geben Sie den Text des Captchas ein',
+	PR_MEDIUM,
+	'Recaptcha (http://recaptcha.net) provides two words in an image, and expects a user to type them in a textfield'
+);
+$lang['de_DE']['RecaptchaField']['VALIDSOLUTION'] = array(
+	'Ihre Antwort hat nicht mit dem Captcha übereingestimmt, bitte probieren Sie es noch einmal',
+	PR_MEDIUM,
+	'Recaptcha (http://recaptcha.net) provides two words in an image, and expects a user to type them in a textfield'
+);
+
+?>
